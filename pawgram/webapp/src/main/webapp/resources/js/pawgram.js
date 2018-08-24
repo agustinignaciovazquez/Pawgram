55a323bc39836ae314a4571592ef9b6a794237ef
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2018-08-24 14:53:13 -0300

    Estilizando y agregando cosas de diseño

--- pawgram/webapp/src/main/webapp/resources/js/pawgram.js
+++ pawgram/webapp/src/main/webapp/resources/js/pawgram.js
@@ -6,7 +6,6 @@ $( document ).ready(function() {
 
 });
 
-
     function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -33.8688, lng: 151.2195},
