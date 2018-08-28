cca31463af868336ad9dea09e9b6625a0984982e
Agustin - <agvazquez@itba.edu.ar>
2018-08-28 05:10:03 -0300

    using modelattributes in register controller

--- pawgram/webapp/src/main/java/ar/edu/itba/pawgram/webapp/controller/RegisterController.java
+++ pawgram/webapp/src/main/java/ar/edu/itba/pawgram/webapp/controller/RegisterController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 @RequestMapping("/register")
@@ -22,26 +23,38 @@ public class RegisterController {
     @Autowired
     private SecurityUserService securityUserService;
 
+    @ModelAttribute("registerForm")
+    public UserForm registerForm() {
+        return new UserForm();
+    }
+
     @RequestMapping(value={"","/"})
-    public ModelAndView register(@ModelAttribute("registerForm") final UserForm form) {
+    public ModelAndView register() {
         return new ModelAndView("register");
     }
 
     @RequestMapping(value = "/process", method = { RequestMethod.POST })
-    public ModelAndView create(@Valid @ModelAttribute("registerForm") final UserForm form, final BindingResult errors) {
+    public ModelAndView create(@ModelAttribute("registerForm") @Valid final UserForm registerUserForm,
+                               final BindingResult errors, final RedirectAttributes attr) {
         if (errors.hasErrors()) {
-            return register(form);
+            return errorState(registerUserForm, errors, attr);
         }
         final User user;
         try {
-            user = securityUserService.registerUser(form.getName(),form.getSurname(),form.getMail(),form.getPasswordForm().getPassword());
+            user = securityUserService.registerUser(registerUserForm.getName(),registerUserForm.getSurname(),registerUserForm.getMail(),registerUserForm.getPasswordForm().getPassword());
         } catch (DuplicateEmailException e) {
             errors.rejectValue("email", "DuplicateEmail");
-            return register(form);
+            return errorState(registerUserForm, errors, attr);
         }
 
         final Authentication auth = new UsernamePasswordAuthenticationToken(user.getMail(), user.getPassword());
         SecurityContextHolder.getContext().setAuthentication(auth);
         return new ModelAndView("redirect:/");
+    }
+
+    private ModelAndView errorState(final UserForm registerUserForm, final BindingResult errors, final RedirectAttributes attr) {
+        attr.addFlashAttribute("org.springframework.validation.BindingResult.registerForm", errors);
+        attr.addFlashAttribute("registerForm", registerUserForm);
+        return new ModelAndView("redirect:/register");
     }
 }
