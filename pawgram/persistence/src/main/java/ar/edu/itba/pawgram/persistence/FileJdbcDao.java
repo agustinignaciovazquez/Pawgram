d0bbe7fb13bafdc088860fb5150b1468b82d8a87
Agustin - <agvazquez@itba.edu.ar>
2018-09-30 17:47:39 -0300

    db fileservice migrated

--- pawgram/persistence/src/main/java/ar/edu/itba/pawgram/persistence/FileJdbcDao.java
+++ pawgram/persistence/src/main/java/ar/edu/itba/pawgram/persistence/FileJdbcDao.java
@@ -4,13 +4,19 @@ import ar.edu.itba.pawgram.interfaces.exception.FileException;
 import ar.edu.itba.pawgram.interfaces.exception.FileUploadException;
 import ar.edu.itba.pawgram.interfaces.persistence.FileDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -18,6 +24,14 @@ import java.util.Map;
 public class FileJdbcDao implements FileDao {
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert jdbcInsert;
+
+    private final static ResultSetExtractor<byte[]> ROW_MAPPER = new ResultSetExtractor<byte[]> () {
+
+        @Override
+        public byte[] extractData(ResultSet rs) throws SQLException, DataAccessException {
+            return rs.next() ? rs.getBytes("data") : null;
+        }
+    };
 
     @Autowired
     public FileJdbcDao(final DataSource ds) {
@@ -41,11 +55,13 @@ public class FileJdbcDao implements FileDao {
 
     @Override
     public byte[] getFile(String path, String fileName) throws FileException {
-        byte[] bytes = jdbcTemplate.queryForObject("SELECT data FROM fileDump WHERE pathId = ? AND imageId = ? ",
-                byte[].class,path,fileName);
-        if(bytes == null)
-            throw new FileException();
-        return bytes;
 
+        byte[] bytes = jdbcTemplate.query("SELECT data FROM fileDump WHERE pathId = ? AND imageId = ? ",
+                ROW_MAPPER, path, fileName);
+
+        if (bytes == null)
+            throw new FileException();
+
+        return bytes;
     }
 }
